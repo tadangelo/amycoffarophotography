ea445a530deb0bb0a9052ceeabd269db34d205ad
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-14 17:38:43 -0600

    Added Heroku fix to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address: "amy_coffaro04@yahoo.com",
+     port: 587,
+     domain: "https://amycoffarophotography.herokuapp.com/",
+     user_name: "<username>",
+     password: "<password>",
+     authentication: 'plain',
+     enable_starttls_auto: true 
+  }
 end
