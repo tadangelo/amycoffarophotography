32927116b17066a53a6ae83af8c57550cb795943
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-14 13:19:21 -0600

    Added  mobile views to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  class MobileFallbackResolver < ::ActionView::FileSystemResolver
+    def find_templates(name, prefix, partial, details)
+      if details[:formats] == [:mobile]
+        # Add a fallback for html, for the case where, eg, 'index.html.haml' exists,
+        # but not 'index.mobile.haml'
+        details = details.dup
+        details[:formats] = [:mobile, :html]
+      end
+      super
+    end
+  end
+  append_view_path MobileFallbackResolver.new('app/views')
 end
