32fbed3dc0efb903cb590d9f496c314ca1e60f3a
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-14 13:14:09 -0600

    Herokufix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :prepend_view_path_if_mobile
-
-      private
-
-      def prepend_view_path_if_mobile
-        if mobile_request?
-          prepend_view_path Rails.root + 'app' + 'mobile_views'
-        end
-      end
-
-      def mobile_request?
-        request.subdomains.first == 'm'
-      end
-      helper_method :mobile_request?
 end
