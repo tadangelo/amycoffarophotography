97ac6ac6e24058db2fc3140e536426482ab01957
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-14 18:44:48 -0600

    Updated from message mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "Your Mailer <amy_coffaro04@yahoo.com>"
+  default from: "Your Mailer #{@message.email}"
   default to: "Your Name <amy_coffaro04@yahoo.com>"
 
   def new_message(message)
