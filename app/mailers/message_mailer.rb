1419690d66b3a4010653b022673a249f2a37f85a
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-14 18:52:09 -0600

    Used mailgun email for heroku fix

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "Your Mailer <#{@message.email}>"
+  default from: "Your Mailer <postmaster@sandbox48b940a3d38a4bf2afea5d04abc33cfc.mailgun.org>"
   default to: "Your Name <amy_coffaro04@yahoo.com>"
 
   def new_message(message)
